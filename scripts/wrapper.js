18ee76c2693d80442ae33cdaae56e2cc475f6abc
Abhishek <abhishek.anand1@sprinklr.com>
2023-06-27 17:10:38 +0530

    removed the division into macrotasks part from last commit

--- scripts/wrapper.js
+++ scripts/wrapper.js
@@ -8,8 +8,8 @@ const getOptimizedCode = require("./helpers/optimizer").default;
 //Path to src
 const relativePathOfFolder = "./src/vibesition/src"; //TODO or directly put absolute path in inputFolderLocation
 const relativePathOfTsConfigFile = "./src/vibesition/tsconfig.json"; //TODO
-const showRunningFile = false;
-const showSmallIterations = false;
+const showRunningFile = true;
+const showSmallIterations = true;
 
 if(fs.existsSync(path.resolve(relativePathOfTsConfigFile)) === false){
   throw "TSConfigNotFound";
@@ -37,62 +37,9 @@ let lastUnusedExportsStringified;
 
 const maxBigIteration = 10, maxSmallIteration = 10;
 let totalBigIteration = 0;
-// while (totalBigIteration<=maxBigIteration) {
-//   totalBigIteration++;
-//   inputFileLocations.forEach((file) => {
-//     try {
-//       const input = fs.readFileSync(file).toString();
-//       if(showRunningFile)console.log(`Running on file ${file}`);
-//       const {output,totalSmallIterations} = getOptimizedCode(input,maxSmallIteration,file);
-//       if(showSmallIterations)console.log(`Iterations: ${totalSmallIterations}\n`);
-//       fs.writeFileSync(file, output.code, "utf8");
-//     }catch(e){
-//       console.log("wrapper.js",{error:e,file:file});
-//     }
-//   });
-//   console.log("\n\n_________________________________________________________________________________\n\n");
-//   //let unusedExportsByFile = analyzeTsConfig(relativePathOfTsConfigFile);
-//   let unusedExportsByFile = analyzeTsConfig(relativePathOfTsConfigFile,["--ignoreLocallyUsed=true"]);
-//   //Processing unusedExportsByFile (ignoring pages folder and if exportName is undefined or empty)
-//   for(let key of Object.keys(unusedExportsByFile)){
-//     if(isUnderAnExcludedDirectory(key)){
-//       delete unusedExportsByFile[key];
-//       continue;
-//     }
-//     unusedExportsByFile[key] = unusedExportsByFile[key].filter(value => !(value.exportName == undefined || value.exportName.length === 0));
-//     if(unusedExportsByFile[key].length === 0)delete unusedExportsByFile[key];
-//   }
 
-//   for(let [fileLocation, unusedExports] of Object.entries(unusedExportsByFile)){
-//     const input = fs.readFileSync(fileLocation).toString();
-//     let output;
-//     try{
-//       if(showRunningFile)console.log(`Removing Exports from file ${fileLocation}`);
-//       output = removeExportsFromFile(input,unusedExports);
-//     }catch(e){
-//       console.log("error while removing exports",{error:e,failSafe: "returned original input instead",file:file});
-//       output = {code: input};
-//     }
-//     fs.writeFileSync(fileLocation, output.code, "utf8");
-//   }
-//   console.log(`\n\n########################################################################################\nEnd of Big Iteration ${totalBigIteration}\n`);
-//   console.log(unusedExportsByFile);
-//   if(Object.keys(unusedExportsByFile).length==0)break;
-
-//   const unusedExportsStringified = JSON.stringify(unusedExportsByFile);
-//   if(lastUnusedExportsStringified!== undefined && unusedExportsStringified === lastUnusedExportsStringified){
-//     console.log("Ending Big Iteration because unable to remove these unused exports", unusedExportsByFile);
-//     break;
-//   }
-//   else {
-//     lastUnusedExportsStringified = unusedExportsStringified;
-//   }
-//   //setTimeout(0)
-// }
-
-function BigIterate(){
+while (totalBigIteration<=maxBigIteration) {
   totalBigIteration++;
-  if(totalBigIteration>maxBigIteration)return;
   inputFileLocations.forEach((file) => {
     try {
       const input = fs.readFileSync(file).toString();
@@ -105,7 +52,7 @@ function BigIterate(){
     }
   });
   console.log("\n\n_________________________________________________________________________________\n\n");
-  //let unusedExportsByFile = analyzeTsConfig(relativePathOfTsConfigFile);
+  // let unusedExportsByFile = analyzeTsConfig(relativePathOfTsConfigFile);
   let unusedExportsByFile = analyzeTsConfig(relativePathOfTsConfigFile,["--ignoreLocallyUsed=true"]);
   //Processing unusedExportsByFile (ignoring pages folder and if exportName is undefined or empty)
   for(let key of Object.keys(unusedExportsByFile)){
@@ -131,23 +78,18 @@ function BigIterate(){
   }
   console.log(`\n\n########################################################################################\nEnd of Big Iteration ${totalBigIteration}\n`);
   console.log(unusedExportsByFile);
-  if(Object.keys(unusedExportsByFile).length==0)return;
+  if(Object.keys(unusedExportsByFile).length==0)break;
 
   const unusedExportsStringified = JSON.stringify(unusedExportsByFile);
   if(lastUnusedExportsStringified!== undefined && unusedExportsStringified === lastUnusedExportsStringified){
     console.log("Ending Big Iteration because unable to remove these unused exports", unusedExportsByFile);
-    return;
+    break;
   }
   else {
     lastUnusedExportsStringified = unusedExportsStringified;
   }
-  setTimeout(BigIterate);
 }
-
-BigIterate();
-
 console.log("totalBigIteration: ",totalBigIteration);
-
 inputFileLocations.forEach((file) => {
   const input = fs.readFileSync(file).toString();
   if (!input.replace(/\s/g, '').length) {
@@ -160,3 +102,136 @@ inputFileLocations.forEach((file) => {
     }
   }
 })
+
+
+
+
+
+
+
+
+
+/////////////////////////////
+/*
+function BigIterate(){
+  totalBigIteration++;
+  if(totalBigIteration>maxBigIteration)return;
+
+
+  let indexOfFile = 0;
+  console.log(inputFileLocations.length);
+
+  function optimizeEachFile(){
+    do{
+      const file = inputFileLocations[indexOfFile];
+      indexOfFile++;
+      try {
+        const input = fs.readFileSync(file).toString();
+        if(showRunningFile)console.log(`Running on file ${file} ::: ${indexOfFile}`);
+        const {output,totalSmallIterations} = getOptimizedCode(input,maxSmallIteration,file);
+        if(showSmallIterations)console.log(`Iterations: ${totalSmallIterations}\n`);
+        fs.writeFileSync(file, output.code, "utf8");
+      }catch(e){
+        console.log("wrapper.js",{error:e,file:file});
+      }
+    }while(indexOfFile!==inputFileLocations.length && indexOfFile%10!=0);
+
+    if(indexOfFile === inputFileLocations.length){
+      setTimeout(removeUnusedExports);
+      return;
+    }
+    else {
+      console.log('\n\n - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n\n')
+      setTimeout(optimizeEachFile);
+    }
+  }
+
+  optimizeEachFile();
+
+  // inputFileLocations.forEach((file) => {
+  //   try {
+  //     const input = fs.readFileSync(file).toString();
+  //     if(showRunningFile)console.log(`Running on file ${file}`);
+  //     const {output,totalSmallIterations} = getOptimizedCode(input,maxSmallIteration,file);
+  //     if(showSmallIterations)console.log(`Iterations: ${totalSmallIterations}\n`);
+  //     fs.writeFileSync(file, output.code, "utf8");
+  //   }catch(e){
+  //     console.log("wrapper.js",{error:e,file:file});
+  //   }
+  // });
+
+  let doNotIterate = false;
+
+  function removeUnusedExports(){
+    console.log("\n\n_________________________________________________________________________________\n\n");
+    //let unusedExportsByFile = analyzeTsConfig(relativePathOfTsConfigFile);
+    let unusedExportsByFile = analyzeTsConfig(relativePathOfTsConfigFile,["--ignoreLocallyUsed=true"]);
+    //Processing unusedExportsByFile (ignoring pages folder and if exportName is undefined or empty)
+    for(let key of Object.keys(unusedExportsByFile)){
+      if(isUnderAnExcludedDirectory(key)){
+        delete unusedExportsByFile[key];
+        continue;
+      }
+      unusedExportsByFile[key] = unusedExportsByFile[key].filter(value => !(value.exportName == undefined || value.exportName.length === 0));
+      if(unusedExportsByFile[key].length === 0)delete unusedExportsByFile[key];
+    }
+
+    for(let [fileLocation, unusedExports] of Object.entries(unusedExportsByFile)){
+      const input = fs.readFileSync(fileLocation).toString();
+      let output;
+      try{
+        if(showRunningFile)console.log(`Removing Exports from file ${fileLocation}`);
+        output = removeExportsFromFile(input,unusedExports);
+      }catch(e){
+        console.log("error while removing exports",{error:e,failSafe: "returned original input instead",file:file});
+        output = {code: input};
+      }
+      fs.writeFileSync(fileLocation, output.code, "utf8");
+    }
+    console.log(`\n\n########################################################################################\nEnd of Big Iteration ${totalBigIteration}\n`);
+    //console.log(unusedExportsByFile);
+    if(Object.keys(unusedExportsByFile).length==0){
+      doNotIterate =  true;
+      return;
+    }
+
+    const unusedExportsStringified = JSON.stringify(unusedExportsByFile);
+    if(lastUnusedExportsStringified!== undefined && unusedExportsStringified === lastUnusedExportsStringified){
+      console.log("Ending Big Iteration because unable to remove these unused exports", unusedExportsByFile);
+      doNotIterate =  true;
+      return;
+    }
+    else {
+      lastUnusedExportsStringified = unusedExportsStringified;
+    }
+    if(doNotIterate){
+      return;
+    }
+    else {
+      setTimeout(BigIterate);
+    }
+  }
+  if(doNotIterate){
+    setTimeout(() => {console.log("totalBigIteration: ",totalBigIteration);});
+    setTimeout(deleteEmptyFiles);
+    return;
+  }
+}
+
+BigIterate();
+
+function deleteEmptyFiles(){
+  inputFileLocations.forEach((file) => {
+    const input = fs.readFileSync(file).toString();
+    if (!input.replace(/\s/g, '').length) {
+      console.log(`Removing empty file: ${file}`);
+      try {
+        fs.unlinkSync(file);
+        console.log("Removed!");
+      }catch(e){
+        console.log(e);
+      }
+    }
+  })
+}
+*/
